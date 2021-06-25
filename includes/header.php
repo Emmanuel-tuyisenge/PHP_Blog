48c1504b49a9baaa4d1bc141db62c9c919982f7c
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-06-25 10:41:34 +0200

    first commit with add arcle form frontend finished

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header>
+    <a href="/" class="logo">Blog</a>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/add_article.php' ? 'active' : '' ?>>
+            <a href="/add_article.php"> Ecrire un article</a>
+        </li>
+    </ul>
+</header>
