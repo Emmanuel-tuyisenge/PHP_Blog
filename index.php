f6691896eb651cb0706e720289749b5e5261166d
Emmanuel-tuyisenge <tuyisenge.emmanuel@yahoo.fr>
2021-06-25 14:54:44 +0200

    Add the logics to display the articles by categories

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+$filename = __DIR__ . '/data/article.json';
+$articles = [];
+$categories = [];
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+    $categoriesMap = array_map(fn ($a) => $a['category'], $articles);
+    $categories = array_reduce($categoriesMap, function ($acc, $cat) {
+        if (isset($acc[$cat])) {
+            $acc[$cat]++;
+        } else {
+            $acc[$cat] = 1;
+        }
+        return $acc;
+    }, []);
+    $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+        if (isset($acc[$article['category']])) {
+            $acc[$article['category']] = [...$acc[$article['category']], $article];
+        } else {
+            $acc[$article['category']] = [$article];
+        }
+        return $acc;
+    }, []);
+    /*echo "<pre>";
+    print_r($articlePerCategories);
+    echo "</pre>";*/
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -11,6 +44,22 @@
     <div class="container">
         <?php require_once 'includes/header.php' ?>
         <div class="content">
+
+            <div class="category-container">
+                <?php foreach ($categories as $cat => $num) : ?>
+                    <h2><?= $cat ?></h2>
+                    <div class="articles-container">
+                        <?php foreach ($articlePerCategories[$cat] as $a) : ?>
+                            <div class="article block">
+                                <div class="overflow">
+                                    <div class="img-container" style="background-image: url(<?= $a['image'] ?>);"></div>
+                                </div>
+                                <h3><?= $a['title'] ?></h3>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
 
         </div>
         <?php require_once 'includes/footer.php' ?>
